58dbf04568ca9a8c0e24a15fe5add6f8d2cdf4bc
Andriy-Kolokolov <koloandre22@gmail.com>
2023-04-19 10:32:21 +0200

    add email check

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const registeredEmailsList = [
+    "exampleemail0@gmail.com",
+    "exampleemail1@gmail.it",
+    "exampleemail2@gmail.de",
+    "exampleemail3@gmail.ru",
+    "exampleemail4@gmail.fr",
+    "exampleemail5@gmail.ch"];
+
+const btnLogin = document.getElementById('btn-login');
+
+btnLogin.addEventListener("click", function (event) {
+    event.preventDefault();
+    const inputEmail = document.getElementById('inputEmail').value;
+
+    if (registeredEmailsList.includes(inputEmail)) {
+        window.location.href = 'game.html'
+    } else {
+        new bootstrap.Popover(btnLogin);
+    }
+});
+
+
+// // for loop solution
+// function emailByLoopCheck() {
+//     for (let i = 0; i < registeredEmailsList.length; i++) {
+//         if (registeredEmailsList.at(i) === inputEmail) {
+//             console.log("EMAIL EXISTS")
+//         }
+//         else {
+//             console.log("EMAIL NOT FOUND")
+//         }
+//     }
+// }
+
